2ab9a15f21489a6ed949205fc8735169693d858f
shotdog <466862016@163.com>
2016-10-10 21:59:01 +0800

    init

--- app/job/invoker.go
+++ app/job/invoker.go
@@ -1,2 +1,37 @@
 package job
 
+import (
+	"github.com/shotdog/quartz"
+	"kitty/app/service"
+)
+
+var jm *JobManager
+type JobManager struct {
+	qz *quartz.Quartz
+}
+
+func NewJobManager()*JobManager {
+
+	if jm == nil {
+
+		qz := quartz.New()
+		qz.BootStrap()
+		jm = &JobManager{qz:qz}
+	}
+
+	return jm
+}
+
+func (this *JobManager)PushAllJob()  {
+
+	list,err := service.JobInfoService.List()
+	if err!= nil || len(list) == 0 {
+		return
+	}
+
+
+	
+
+
+
+}
